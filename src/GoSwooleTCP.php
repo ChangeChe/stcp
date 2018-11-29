6909a09fc12a90c3fadeda6cd26f1c6593adc108
change <wannmb@hotmail.com>
2018-11-29 17:59:04 +0800

    自动注册类

--- src/GoSwooleTCP.php
+++ src/GoSwooleTCP.php
@@ -11,7 +11,7 @@ $input = file_get_contents('php://stdin');
 $cfg = json_decode($input, true);
 
 spl_autoload_register(function ($class) {
-    $file = __DIR__ . '/' . substr(str_replace('\\', '/', $class), 16) . '.php';
+    $file = __DIR__ . '/' . substr(str_replace('\\', '/', $class), 13) . '.php';
     if (is_readable($file)) {
         require $file;
         return true;
